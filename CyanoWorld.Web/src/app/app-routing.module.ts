45146b58b8f4c1a5fbacc90f1b26d2250be1abb4
vinay faye <vinayfaye1@gmail.com>
2023-12-16 22:24:18 +0530

    Added initial layout

--- CyanoWorld.Web/src/app/app-routing.module.ts
+++ CyanoWorld.Web/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './modules/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'Dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'Dashboard',
+        loadChildren: () =>
+          import('./modules/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          )
+      }
+
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CyanoWorld.Web/src/app/app.module.ts
+++ CyanoWorld.Web/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './modules/layout/header/header.component';
+import { FooterComponent } from './modules/layout/footer/footer.component';
+import { LayoutComponent } from './modules/layout/layout.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
